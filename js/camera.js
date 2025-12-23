19dd7bbf9224bfbb0fbb8dba292e28a8477d8d26
SulthanHA <sulthandyno@gmail.com>
2025-12-23 12:43:34 +0700

    refactor; camera js

--- js/camera.js
+++ js/camera.js
@@ -1,35 +1,39 @@
+// State Kamera
 let videoElement, canvasElement, canvasContext;
 let streamActive = null;
 let isCameraActive = false;
 let currentVideoFrame = null;
+// menyiapkan camera
 
+// Inisialisasi Sistem
 export function initCamera(video, canvas, ctx){
   videoElement = video;
   canvasElement = canvas;
   canvasContext = ctx;
-  canvasContext.willReadFrequently = true;
+  canvasContext.willReadFrequently = true; //canvas ini sering dibaca pixel-nya
 }
 
 export async function startCameraStream(){
   try{
     const constraints = {
       video:{
-        width:{ideal:1280, min:640},
-        height:{ideal:720, min:360},
+        width:{ideal:1280, min:640}, //ukuran lebar resolusi video
+        height:{ideal:720, min:360}, //ukuran tinggi resolusi video
         facingMode:'environment'
       },
-      audio:false
+      audio:false //tidak menggunakan audio
     };
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    const stream = await navigator.mediaDevices.getUserMedia(constraints); //untuk mengakses perangkat multimedia (kamera
     streamActive = stream;
-    videoElement.srcObject = stream;
+    videoElement.srcObject = stream; //Menghubungkan Kamera
     isCameraActive = true;
 
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject)=>{ //Error Handling
       videoElement.onloadedmetadata = ()=>{
+        //Sinkronisasi Resolusi Video & Canvas
         canvasElement.width = videoElement.videoWidth;
         canvasElement.height = videoElement.videoHeight;
-        videoElement.play().then(resolve).catch(e=>{
+        videoElement.play().then(resolve).catch(e=>{ // Memulai pemutaran video
           console.error("Error playing video:", e);
           isCameraActive=false; reject(new Error("Gagal memutar stream video."));
         });
@@ -53,14 +57,14 @@ export async function startCameraStream(){
 
 export function stopCameraStream(){
   if (streamActive){
-    streamActive.getTracks().forEach(t=>t.stop());
+    streamActive.getTracks().forEach(t=>t.stop()); //Matikan Kamera
     videoElement.srcObject=null;
     videoElement.pause();
     streamActive=null; isCameraActive=false; currentVideoFrame=null;
   }
 }
 
-export function drawLiveFrame(filterCallback){
+export function drawLiveFrame(filterCallback){ //Filter Callback (Real-Time Processing) frame kamera - copy pixel - filter rgb - tampil ke canvas
   if (!isCameraActive || videoElement.readyState < videoElement.HAVE_ENOUGH_DATA) return null;
   canvasContext.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
   currentVideoFrame = canvasContext.getImageData(0, 0, canvasElement.width, canvasElement.height);
